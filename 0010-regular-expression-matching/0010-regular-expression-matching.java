6f5291c9842418f951f6d2fd0061de31cd0fdc38
Doa'a OsamaK <147305995+DoaaOsamaK@users.noreply.github.com>
2023-12-21 11:03:29 +0200

    Time: 964 ms (15.36%), Space: 45.4 MB (6.26%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(p.length() == 0)
+           return s.length() == 0;
+        if(p.length() > 1 && p.charAt(1) == '*'){
+            if(isMatch(s, p.substring(2)))
+               return true;
+            if(s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0)))
+                return isMatch(s.substring(1), p);
+            return false;
+        }else{
+            if(s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0)))
+                return isMatch(s.substring(1), p.substring(1));
+        }
+        return false;
+    }
+}
