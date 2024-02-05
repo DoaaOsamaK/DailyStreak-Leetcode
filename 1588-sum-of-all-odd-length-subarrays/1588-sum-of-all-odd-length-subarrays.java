8401450a00979de548555e216c8ca01e992ef2f4
Doa'a OsamaK <147305995+DoaaOsamaK@users.noreply.github.com>
2024-02-05 12:01:40 +0200

    Time: 0 ms (100.00%), Space: 41.2 MB (50.51%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -0,0 +1,9 @@
+class Solution {
+     public int sumOddLengthSubarrays(int[] A) {
+        int res = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * A[i];
+        }
+        return res;
+    }
+}
